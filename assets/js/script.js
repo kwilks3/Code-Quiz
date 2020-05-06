df80eb110b7e948bbf382f11879611012f2dbd40
Kimberly Wilks <wilks.kimberly@gmail.com>
2020-05-05 21:55:48 -0500

    added pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//create timer that starts on click of start button
+//output first question on start button click
+//create loop for answering questions and outputting new questions
+// display whether answer is correct or incorrect for two seconds
+// if answer is incorrect subtract 5 seconds from timer
+//when all questions are answered or timer is 0 end quiz
+// ask for initials
+// display scoreboard with initials and scores
